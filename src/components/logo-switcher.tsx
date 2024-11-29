2b9159bb2bc1386d13be90543a595244394a5ba1
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-11-29 08:18:23 +0100

    Using motion instead of framer-motion & updating deps

--- src/components/logo-switcher.tsx
+++ src/components/logo-switcher.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "motion/react";
 
 const LogoSwitcher = () => {
   const [currentLogo, setCurrentLogo] = useState("vercel");
