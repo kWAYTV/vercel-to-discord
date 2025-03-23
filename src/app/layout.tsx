a1e93537c7a026e06eaf9728976bdfc7dde3f8bc
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-03-23 23:25:09 +0100

    feat(theme): integrate ThemeProvider for improved theming support
    - Added ThemeProvider to layout for managing themes.
    - Updated layout structure to support theme switching and system preferences.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import { AnimatedGridPattern } from "@/components/magicui/animated-grid-pattern";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -37,20 +38,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en" className={inter.className} suppressHydrationWarning>
       <body>
-        <div className="relative min-h-screen overflow-hidden">
-          <AnimatedGridPattern
-            maxOpacity={0.1}
-            duration={3}
-            repeatDelay={1}
-            className={cn(
-              "[mask-image:radial-gradient(650px_circle_at_center,white,transparent)]",
-              "absolute inset-0 -z-10"
-            )}
-          />
-          {children}
-        </div>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="relative min-h-screen overflow-hidden">
+            <AnimatedGridPattern
+              maxOpacity={0.1}
+              duration={3}
+              repeatDelay={1}
+              className={cn(
+                "[mask-image:radial-gradient(650px_circle_at_center,white,transparent)]",
+                "absolute inset-0 -z-10"
+              )}
+            />
+            {children}
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
