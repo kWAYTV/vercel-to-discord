25485764a76aa0c2ea19849d556f696b037d78d1
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-03-27 07:14:45 +0100

    chore(config): remove ecosystem.config.js and update next.config.ts for image handling
    - Deleted ecosystem.config.js as it is no longer needed.
    - Added remote image patterns in next.config.ts to support external image sources.

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pbgegowfpr.ufs.sh",
+        port: "",
+        pathname: "/f/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
