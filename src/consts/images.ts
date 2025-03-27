e96e0fcce7895b57a9e25d6bbf5d1f1211f4ec05
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-03-27 07:14:17 +0100

    feat(back-to-top, images): add BackToTop component and image constants
    - Implemented BackToTop component for smooth scrolling to the top of the page.
    - Added image constants for Discord webhook examples and setup instructions.
    - Defined types for image objects to ensure type safety.

---
+++ src/consts/images.ts
@@ -0,0 +1,16 @@
+import { DocImage, DocImages } from "@/types/images";
+
+export const docImages: DocImages = {
+  discordWebhookExample: {
+    src: "https://pbgegowfpr.ufs.sh/f/5Ajhdz1iWCoOvQQd7AZPbPwa8o1hxYgHdu5VtCizUOKsrmcA",
+    alt: "Discord Webhook Example",
+  },
+  createDiscordWebhook: {
+    src: "https://pbgegowfpr.ufs.sh/f/5Ajhdz1iWCoOqCtS5ozZobQTyXmEqudMpnZIcB8hgk3jw96H",
+    alt: "Create Discord Webhook",
+  },
+  setupVercelWebhook: {
+    src: "https://pbgegowfpr.ufs.sh/f/5Ajhdz1iWCoOFC7MeJptgNcYPhDMdF4apry536Vj1AUzvS8K",
+    alt: "Setup Vercel Webhook",
+  },
+};
---
+++ src/types/images.ts
@@ -0,0 +1,10 @@
+export type DocImage = {
+  src: string;
+  alt: string;
+};
+
+export interface DocImages {
+  discordWebhookExample: DocImage;
+  createDiscordWebhook: DocImage;
+  setupVercelWebhook: DocImage;
+}
